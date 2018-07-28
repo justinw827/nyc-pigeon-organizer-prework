39a1a7687fb909a1309636a03808ab9fffe34001
Justin Wang <justinw827@aol.com>
2018-07-28 04:24:47 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  newData = Hash.new
+
+  data.each do |key, val|
+    val.each do |key2, val2|
+      val2.each do |pigeon|
+        if newData.include?(pigeon)
+      end
+    end
+  end
+
+  return newData
 end
