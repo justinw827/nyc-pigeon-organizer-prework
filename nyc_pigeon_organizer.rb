bfc6ad8bea96be79144d109765edab5df27900a5
Justin Wang <justinw827@aol.com>
2018-07-28 04:42:24 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,9 +7,10 @@ def nyc_pigeon_organizer(data)
     val.each do |key2, val2|
       val2.each do |pigeon|
         if newData.include?(pigeon) # Check if pigeon exists in newHash
-          binding.pry
+          #binding.pry
           if newData[pigeon].include?(key) # Check if pigeon has the category yet
             newData[pigeon][key] << key2.to_s
+            binding.pry
           else
             newData[pigeon] = {key => [key2.to_s]}
           end
